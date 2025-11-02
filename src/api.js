c85a65513ed9d7fd94aa154af79fdb2e213232a3
nagarajan13172 <haridiablo72@gmail.com>
2025-11-02 22:59:17 +0000

    Update API_BASE and RU constants to use environment variables for better configuration management

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-export const API_BASE = 'http://localhost:3000/api/graduation';
-export const RU = 'http://localhost:3000';
+export const API_BASE = import.meta.env.VITE_API_URL;
+export const RU = import.meta.env.VITE_RU;
 
 // export const API_BASE = 'http://14.139.186.91:3000/api/graduation';
 
